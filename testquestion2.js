34ad1076d805244347a80a2fae179892e30c8822
chialucas <chialucas56@gmail.com>
2025-08-30 15:16:36 +0800

    Update testquestion2.js

--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,13 @@
+function setup(){
+    createCanvas(600,600);// it create a canvas of the size given
+    background(220);
+}
+function draw() {
+    for(let i=1; i < 11; i++){
+        fill(i* 25);
+        circle(30 + i *50, 25 + i*50, 50);
+}
+
+}
+
 
