e3f84a27a297182d7d7a4228b2c3b269ea277423
chialucas <chialucas56@gmail.com>
2025-08-30 15:16:17 +0800

    Update testquestion1.js

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,18 @@
+function setup() {
+    createCanvas(600,400);
+    background(0);
+    stroke("red");
+    strokeWeight(10);
+}
+
+function draw() {
+    fill("black");
+    ellipse(150,200,100,100);
+    fill("black");
+    triangle(250,250,300,150,350,250);
+    fill("black");
+    rect(400,150,100,100);
+}
+
+
 
